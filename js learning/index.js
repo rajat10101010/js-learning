6f9a27d1f56f9134882dc50aee5e2f5f047aa0e6
Rajat Kuchara <tirthkuchara@gmail.com>
2024-02-27 10:00:58 +0530

    basic js programm

---
+++ js learning/index.js
@@ -0,0 +1,13 @@
+let color = "Green"
+
+if (color === "red") {
+    console.log("stop! Color is red");
+}
+
+if (color === "Yellow") {
+    console.log("Start! Color is Yellow");
+}
+
+if (color === "Green") {
+    console.log("Go! Color is Green");
+}
