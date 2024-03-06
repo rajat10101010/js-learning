aebeeec7a766d6c46dbcb0185816ef33ab6dcdc7
Rajat R Kuchara <tirthkuchara@gmail.com>
2024-03-06 14:24:00 +0530

    Basic guessing game by js

---
+++ js learning/guessing.js
@@ -0,0 +1,25 @@
+const max = prompt("Enter the max number");
+
+const random = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("Guess the number");
+
+while(true) {
+    if(guess == "quit") {
+        console.log("user quit");
+        break;
+    }
+
+    if(guess == random) {
+        console.log("you are right! congrats!!");
+        break;
+    }
+
+    else if(guess < random) {
+        guess = prompt("Hint : your guess was too small. please try again...")
+    }
+    
+    else {
+        guess = prompt("Hint : your guess was too learg. please try again...")
+    }
+}
